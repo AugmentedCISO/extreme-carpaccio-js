dc3b5601d134838be1f4969c696345935ce2a075
Antoine Scherrer <picakoch@gmail.com>
2021-10-20 17:38:28 +0200

    add ping

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
+const app = express();
 app.use(express.json());
 
-app.post('/order', (req,res) => {
-  console.log(req.body)
-  res.status(200).json({total: 0})
-})
+app.post("/order", (req, res) => {
+  console.log(req.body);
+  // Your code here
+  res.status(200).json({ total: 0 });
+});
 
-app.post('/feedback', (req,res) => {
-  console.log(req.body)
-  res.status(200)
-})
+app.post("/feedback", (req, res) => {
+  console.log(req.body);
+  res.status(200);
+});
+
+app.post("/ping", (req, res) => {
+  console.log(req.body);
+  res.status(200).body("pong");
+});
 
 app.listen(3051, () => {
-  console.log('Server started')
-})
+  console.log("Server started");
+});
